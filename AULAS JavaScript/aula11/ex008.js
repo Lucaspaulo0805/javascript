fbf3cf087d37c6786c89c196c4a2cc60215b33a5
Lucas Paulo <94088741+Lucaspaulo0805@users.noreply.github.com>
2022-05-04 10:43:30 -0300

    atuali

--- AULAS JavaScript/aula11/ex008.js
+++ AULAS JavaScript/aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 60
 console.log(`Avelocidade do seu carro é ${vel}Km/h`)
-    if (vel > 60) {
+    if (vel > 60) { //Condição Simples!
         console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
     }
-    console.log(`Dirija sempre usando cinto de segurança!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
--- AULAS JavaScript/aula11/ex009.js
+++ AULAS JavaScript/aula11/ex009.js
@@ -1,4 +1,4 @@
-var pais = 'USA'
+var pais = 'Brasil'
 console.log(`Vivendo em ${pais}`)
 if (pais == 'Brasil') {
     console.log('Você é Brasileiro(a)!')
